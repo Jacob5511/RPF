d4920476683996f34ee767091dde72f489bb38b8
YakovIVR <116104066+YakovIVR@users.noreply.github.com>
2023-11-06 19:34:00 +0300

    Update BetweenScenes.cs

--- RPF/Assets/Scripts/BetweenScenes.cs
+++ RPF/Assets/Scripts/BetweenScenes.cs
@@ -5,69 +5,69 @@ using System.Collections.Generic;
 
 public static class BetweenScenes
 {
-    public static string recipes_id { get; set; } // ID рецепта
-    public static Texture texture { get; set; } // Текстура
-    public static RawImage image { get; set; } // Объект для отображения изображения
-    public static ImagePathScripts image_path_scripts { get; set; } // Сценарий для управления путями изображений
-    public static string recipe_name { get; set; } // Название рецепта
-    public static string search_bar { get; set; } // Поисковый запрос
-    public static string rating_from_one { get; set; } // Рейтинг от (1)
-    public static string rating_from_two { get; set; } // Рейтинг от (2)
-    public static string rating_to_one { get; set; } // Рейтинг до (1)
-    public static string rating_to_two { get; set; } // Рейтинг до (2)
-    public static string views_from { get; set; } // Просмотры от
-    public static string views_to { get; set; } // Просмотры до
-    public static string name { get; set; } // Имя пользователя
-    public static bool can_i_make_recipe { get; set; } // Можно ли готовить рецепт
-    public static bool sort_by_views { get; set; } // Сортировка по просмотрам
-    public static bool is_my_recipes { get; set; } // Это мои рецепты
-    public static bool is_downloaded { get; set; } // Файл рецепта загружен
-    public static float aspect_ratio { get; set; } // Соотношение сторон
-    public static string description { get; set; } // Описание
-    public static string ingredients { get; set; } // Ингредиенты
-    public static string timers { get; set; } // Таймеры
-    public static string links_to_pictures { get; set; } // Ссылки на изображения
-    public static string steps_descriptions { get; set; } // Описания шагов
-    public static bool is_from_about_recipe { get; set; } // Открыто из "О рецепте"
-    public static string recipe_file_name { get; set; } // Имя файла рецепта
-    public static GameObject units { get; set; } // Объекты (юниты)
-    public static string search_bar_find_chief { get; set; } // Поисковый запрос для поиска шеф-повара
-    public static string rating_from_one_find_chief { get; set; } // Рейтинг от (1) для поиска шеф-повара
-    public static string rating_from_two_find_chief { get; set; } // Рейтинг от (2) для поиска шеф-повара
-    public static string rating_to_one_find_chief { get; set; } // Рейтинг до (1) для поиска шеф-повара
-    public static string rating_to_two_find_chief { get; set; } // Рейтинг до (2) для поиска шеф-повара
-    public static string views_from_find_chief { get; set; } // Просмотры от для поиска шеф-повара
-    public static string views_to_find_chief { get; set; } // Просмотры до для поиска шеф-повара
-    public static string metro_find_chief { get; set; } // Метро для поиска шеф-повара
-    public static float vertical_normalized_position { get; set; } = 1; // Вертикальная нормализованная позиция
-    public static List<string> sp_timers { get; set; } // Таймеры для шагов
-    public static string old_request { get; set; } // Старый запрос
-    public static string search_data { get; set; } // Данные для поиска
-    public static string metro { get; set; } // Метро
-    public static string price_from { get; set; } // Цена от
-    public static string price_to { get; set; } // Цена до
+    public static string recipes_id { get; set; } // ID СЂРµС†РµРїС‚Р°
+    public static Texture texture { get; set; } // РўРµРєСЃС‚СѓСЂР°
+    public static RawImage image { get; set; } // РћР±СЉРµРєС‚ РґР»СЏ РѕС‚РѕР±СЂР°Р¶РµРЅРёСЏ РёР·РѕР±СЂР°Р¶РµРЅРёСЏ
+    public static ImagePathScripts image_path_scripts { get; set; } // РЎС†РµРЅР°СЂРёР№ РґР»СЏ СѓРїСЂР°РІР»РµРЅРёСЏ РїСѓС‚СЏРјРё РёР·РѕР±СЂР°Р¶РµРЅРёР№
+    public static string recipe_name { get; set; } // РќР°Р·РІР°РЅРёРµ СЂРµС†РµРїС‚Р°
+    public static string search_bar { get; set; } // РџРѕРёСЃРєРѕРІС‹Р№ Р·Р°РїСЂРѕСЃ
+    public static string rating_from_one { get; set; } // Р РµР№С‚РёРЅРі РѕС‚ (1)
+    public static string rating_from_two { get; set; } // Р РµР№С‚РёРЅРі РѕС‚ (2)
+    public static string rating_to_one { get; set; } // Р РµР№С‚РёРЅРі РґРѕ (1)
+    public static string rating_to_two { get; set; } // Р РµР№С‚РёРЅРі РґРѕ (2)
+    public static string views_from { get; set; } // РџСЂРѕСЃРјРѕС‚СЂС‹ РѕС‚
+    public static string views_to { get; set; } // РџСЂРѕСЃРјРѕС‚СЂС‹ РґРѕ
+    public static string name { get; set; } // РРјСЏ РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ
+    public static bool can_i_make_recipe { get; set; } // РњРѕР¶РЅРѕ Р»Рё РіРѕС‚РѕРІРёС‚СЊ СЂРµС†РµРїС‚
+    public static bool sort_by_views { get; set; } // РЎРѕСЂС‚РёСЂРѕРІРєР° РїРѕ РїСЂРѕСЃРјРѕС‚СЂР°Рј
+    public static bool is_my_recipes { get; set; } // Р­С‚Рѕ РјРѕРё СЂРµС†РµРїС‚С‹
+    public static bool is_downloaded { get; set; } // Р¤Р°Р№Р» СЂРµС†РµРїС‚Р° Р·Р°РіСЂСѓР¶РµРЅ
+    public static float aspect_ratio { get; set; } // РЎРѕРѕС‚РЅРѕС€РµРЅРёРµ СЃС‚РѕСЂРѕРЅ
+    public static string description { get; set; } // РћРїРёСЃР°РЅРёРµ
+    public static string ingredients { get; set; } // РРЅРіСЂРµРґРёРµРЅС‚С‹
+    public static string timers { get; set; } // РўР°Р№РјРµСЂС‹
+    public static string links_to_pictures { get; set; } // РЎСЃС‹Р»РєРё РЅР° РёР·РѕР±СЂР°Р¶РµРЅРёСЏ
+    public static string steps_descriptions { get; set; } // РћРїРёСЃР°РЅРёСЏ С€Р°РіРѕРІ
+    public static bool is_from_about_recipe { get; set; } // РћС‚РєСЂС‹С‚Рѕ РёР· "Рћ СЂРµС†РµРїС‚Рµ"
+    public static string recipe_file_name { get; set; } // РРјСЏ С„Р°Р№Р»Р° СЂРµС†РµРїС‚Р°
+    public static GameObject units { get; set; } // РћР±СЉРµРєС‚С‹ (СЋРЅРёС‚С‹)
+    public static string search_bar_find_chief { get; set; } // РџРѕРёСЃРєРѕРІС‹Р№ Р·Р°РїСЂРѕСЃ РґР»СЏ РїРѕРёСЃРєР° С€РµС„-РїРѕРІР°СЂР°
+    public static string rating_from_one_find_chief { get; set; } // Р РµР№С‚РёРЅРі РѕС‚ (1) РґР»СЏ РїРѕРёСЃРєР° С€РµС„-РїРѕРІР°СЂР°
+    public static string rating_from_two_find_chief { get; set; } // Р РµР№С‚РёРЅРі РѕС‚ (2) РґР»СЏ РїРѕРёСЃРєР° С€РµС„-РїРѕРІР°СЂР°
+    public static string rating_to_one_find_chief { get; set; } // Р РµР№С‚РёРЅРі РґРѕ (1) РґР»СЏ РїРѕРёСЃРєР° С€РµС„-РїРѕРІР°СЂР°
+    public static string rating_to_two_find_chief { get; set; } // Р РµР№С‚РёРЅРі РґРѕ (2) РґР»СЏ РїРѕРёСЃРєР° С€РµС„-РїРѕРІР°СЂР°
+    public static string views_from_find_chief { get; set; } // РџСЂРѕСЃРјРѕС‚СЂС‹ РѕС‚ РґР»СЏ РїРѕРёСЃРєР° С€РµС„-РїРѕРІР°СЂР°
+    public static string views_to_find_chief { get; set; } // РџСЂРѕСЃРјРѕС‚СЂС‹ РґРѕ РґР»СЏ РїРѕРёСЃРєР° С€РµС„-РїРѕРІР°СЂР°
+    public static string metro_find_chief { get; set; } // РњРµС‚СЂРѕ РґР»СЏ РїРѕРёСЃРєР° С€РµС„-РїРѕРІР°СЂР°
+    public static float vertical_normalized_position { get; set; } = 1; // Р’РµСЂС‚РёРєР°Р»СЊРЅР°СЏ РЅРѕСЂРјР°Р»РёР·РѕРІР°РЅРЅР°СЏ РїРѕР·РёС†РёСЏ
+    public static List<string> sp_timers { get; set; } // РўР°Р№РјРµСЂС‹ РґР»СЏ С€Р°РіРѕРІ
+    public static string old_request { get; set; } // РЎС‚Р°СЂС‹Р№ Р·Р°РїСЂРѕСЃ
+    public static string search_data { get; set; } // Р”Р°РЅРЅС‹Рµ РґР»СЏ РїРѕРёСЃРєР°
+    public static string metro { get; set; } // РњРµС‚СЂРѕ
+    public static string price_from { get; set; } // Р¦РµРЅР° РѕС‚
+    public static string price_to { get; set; } // Р¦РµРЅР° РґРѕ
 
 }
 
 [System.Serializable]
 public class User
 {
-    public string username; // Имя пользователя
-    public string hash; // Хеш пароля
-    public int count; // Счетчик
-    public string email; // Электронная почта
+    public string username; // РРјСЏ РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ
+    public string hash; // РҐРµС€ РїР°СЂРѕР»СЏ
+    public int count; // РЎС‡РµС‚С‡РёРє
+    public string email; // Р­Р»РµРєС‚СЂРѕРЅРЅР°СЏ РїРѕС‡С‚Р°
 }
 
 [System.Serializable]
 public class Chief
 {
-    public string foto_path; // Путь к фото
-    public string name; // Имя
-    public string views; // Просмотры
-    public string score; // Рейтинг
-    public string phone_number; // Номер телефона
-    public string metro; // Метро
-    public string short_about_chief; // Краткая информация о шеф-поваре
-    public string add_or_remove; // Добавить или удалить
+    public string foto_path; // РџСѓС‚СЊ Рє С„РѕС‚Рѕ
+    public string name; // РРјСЏ
+    public string views; // РџСЂРѕСЃРјРѕС‚СЂС‹
+    public string score; // Р РµР№С‚РёРЅРі
+    public string phone_number; // РќРѕРјРµСЂ С‚РµР»РµС„РѕРЅР°
+    public string metro; // РњРµС‚СЂРѕ
+    public string short_about_chief; // РљСЂР°С‚РєР°СЏ РёРЅС„РѕСЂРјР°С†РёСЏ Рѕ С€РµС„-РїРѕРІР°СЂРµ
+    public string add_or_remove; // Р”РѕР±Р°РІРёС‚СЊ РёР»Рё СѓРґР°Р»РёС‚СЊ
 }
 
